1ec8dcb365c7a4e784534f249d4caa8c9e0fb519
ssawaftah <74429413+ssawaftah@users.noreply.github.com>
2026-05-13 05:58:08 +0300

    Add user access management routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getUsers,
-  createUser,
-} = require("../controllers/user.controller");
-
+const { getUsers, createUser, updateUserAccess, getMyAccess } = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const roleMiddleware = require("../middlewares/role.middleware");
 
+router.get("/me/access", authMiddleware, getMyAccess);
 router.get("/", authMiddleware, roleMiddleware("admin"), getUsers);
 router.post("/", authMiddleware, roleMiddleware("admin"), createUser);
+router.put("/:id/access", authMiddleware, roleMiddleware("admin"), updateUserAccess);
+router.patch("/:id/access", authMiddleware, roleMiddleware("admin"), updateUserAccess);
 
 module.exports = router;

3c6ac374cb13a02c94533b41e4bc9d27ef24fca2
ssawaftah <74429413+ssawaftah@users.noreply.github.com>
2026-05-14 08:14:35 +0300

    Normalize stored request values before request actions

--- backend/routes/leave.routes.js
+++ backend/routes/leave.routes.js
@@ -12,12 +12,13 @@ const {
 const authMiddleware = require("../middlewares/auth.middleware");
 const permissionMiddleware = require("../middlewares/permission.middleware");
 const { normalizeRequestPayload } = require("../middlewares/request-normalizer.middleware");
+const { normalizeStoredRequestPayload } = require("../middlewares/stored-request-normalizer.middleware");
 
 router.get("/", authMiddleware, permissionMiddleware("requests.view.self", "requests.view.department", "requests.view.all", "requests.manage"), getRequests);
-router.get("/:id", authMiddleware, permissionMiddleware("requests.view.self", "requests.view.department", "requests.view.all", "requests.manage"), getRequestById);
+router.get("/:id", authMiddleware, normalizeStoredRequestPayload, permissionMiddleware("requests.view.self", "requests.view.department", "requests.view.all", "requests.manage"), getRequestById);
 router.post("/", authMiddleware, normalizeRequestPayload, permissionMiddleware("requests.create.self", "requests.manage"), createRequest);
-router.patch("/:id/action", authMiddleware, normalizeRequestPayload, permissionMiddleware("requests.cancel.self_pending", "requests.approve.department", "requests.approve.all", "requests.reject.department", "requests.reject.all", "requests.cancel.department", "requests.cancel.all", "requests.request_info", "requests.comment", "requests.manage"), actOnRequest);
-router.put("/:id/action", authMiddleware, normalizeRequestPayload, permissionMiddleware("requests.cancel.self_pending", "requests.approve.department", "requests.approve.all", "requests.reject.department", "requests.reject.all", "requests.cancel.department", "requests.cancel.all", "requests.request_info", "requests.comment", "requests.manage"), actOnRequest);
+router.patch("/:id/action", authMiddleware, normalizeRequestPayload, normalizeStoredRequestPayload, permissionMiddleware("requests.cancel.self_pending", "requests.approve.department", "requests.approve.all", "requests.reject.department", "requests.reject.all", "requests.cancel.department", "requests.cancel.all", "requests.request_info", "requests.comment", "requests.manage"), actOnRequest);
+router.put("/:id/action", authMiddleware, normalizeRequestPayload, normalizeStoredRequestPayload, permissionMiddleware("requests.cancel.self_pending", "requests.approve.department", "requests.approve.all", "requests.reject.department", "requests.reject.all", "requests.cancel.department", "requests.cancel.all", "requests.request_info", "requests.comment", "requests.manage"), actOnRequest);
 router.delete("/:id", authMiddleware, permissionMiddleware("requests.manage"), deleteRequest);
 
 module.exports = router;

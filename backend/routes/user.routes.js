2131eb3bed3d65a5fddbffb86517c579b77ee4db
ssawaftah <74429413+ssawaftah@users.noreply.github.com>
2026-05-15 19:19:35 +0300

    Register self account security route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUsers, createUser, updateUserAccess, getMyAccess } = require("../controllers/user.controller");
+const { getUsers, createUser, updateUserAccess, getMyAccess, updateMyCredential } = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const roleMiddleware = require("../middlewares/role.middleware");
 
 router.get("/me/access", authMiddleware, getMyAccess);
+router.put("/me/security", authMiddleware, updateMyCredential);
+router.patch("/me/security", authMiddleware, updateMyCredential);
 router.get("/", authMiddleware, roleMiddleware("admin"), getUsers);
 router.post("/", authMiddleware, roleMiddleware("admin"), createUser);
 router.put("/:id/access", authMiddleware, roleMiddleware("admin"), updateUserAccess);
